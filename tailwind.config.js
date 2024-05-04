84bf47b75a7e665de425b21ba0c63aa1bfa9771e
souvik27-lab <158319175+souvik27-lab@users.noreply.github.com>
2024-05-04 23:07:53 +0530

    4-5-2024 hero section complete 1st commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize:{
+        sm1:"0.75rem",
+        sm2:"0.875rem",
+        sm3:"1rem",
+        sm4:"1.25rem",
+        sm5:"1.5rem",
+        sm6:"1.75rem",
+        sm7:"2.25rem",
+      }
+    },
   },
   plugins: [],
 }
